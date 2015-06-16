8ec6f16add835cc7743f70ea7fb3b46acc6dc262
Joseph Redmon <pjreddie@gmail.com>
2015-06-16 14:54:26 -0700

    width and stuff

--- src/image.c
+++ src/image.c
@@ -59,6 +59,14 @@ void draw_box(image a, int x1, int y1, int x2, int y2, float r, float g, float b
     }
 }
 
+void draw_box_width(image a, int x1, int y1, int x2, int y2, int w, float r, float g, float b)
+{
+    int i;
+    for(i = 0; i < w; ++i){
+        draw_box(a, x1+i, y1+i, x2-i, y2-i, r, g, b);
+    }
+}
+
 void flip_image(image a)
 {
     int i,j,k;
