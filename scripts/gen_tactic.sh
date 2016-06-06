e4a2e02443f2af6bea96738728185ba653ae1716
Joseph Redmon <pjreddie@gmail.com>
2016-06-06 13:33:08 -0700

    example

--- scripts/gen_tactic.sh
+++ scripts/gen_tactic.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+# Usage:
+# wget http://pjreddie.com/media/files/peek.weights
+# scripts/gen_tactic.sh data/goal.txt
 ./darknet rnn generatetactic cfg/gru.cfg peek.weights < $1 2>/dev/null
