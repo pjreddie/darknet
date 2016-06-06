d790f21c9a56cc2eadb4f3ee5d3aed7f7b677178
Joseph Redmon <pjreddie@gmail.com>
2016-06-06 13:37:30 -0700

    damnit alex

--- scripts/gen_tactic.sh
+++ scripts/gen_tactic.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Usage:
 # wget http://pjreddie.com/media/files/peek.weights
-# scripts/gen_tactic.sh data/goal.txt
-./darknet rnn generatetactic cfg/gru.cfg peek.weights < $1 2>/dev/null
+# scripts/gen_tactic.sh < data/goal.txt
+./darknet rnn generatetactic cfg/gru.cfg peek.weights 2>/dev/null
