b411c843b4b200e2a2dab8f45dc47ca29bac64b6
stefan <think@hotmail.de>
2016-12-16 20:12:24 +0100

    cleaned up

--- scripts/darknet.sh
+++ scripts/darknet.sh
@@ -3,4 +3,3 @@
 set -e
 cat - > /tmp/image
 /darknet/darknet $@ /tmp/image 2>/dev/null | grep ': [0-9]'
-# /darknet/darknet classifier predict cfg/imagenet22k.dataset cfg/extraction.cfg /extraction.weights /tmp/image 2>/dev/null | grep ': [0-9]'
