1e729804f61c8627eb257fba8b83f74e04945db7
Joseph Redmon <pjreddie@gmail.com>
2017-08-03 04:31:04 -0700

    OK GOD DAMN IT I THINK IT'S RIGHT NOW WHATEVER

--- examples/detector.py
+++ examples/detector.py
@@ -8,7 +8,7 @@ sys.path.append(os.path.join(os.getcwd(),'python/'))
 
 import darknet as dn
 
-net = dn.load_net("cfg/tiny-yolo.cfg", "tiny-yolo.backup", 0)
+net = dn.load_net("cfg/tiny-yolo.cfg", "tiny-yolo.weights", 0)
 meta = dn.load_meta("cfg/coco.data")
 r = dn.detect(net, meta, "data/dog.jpg")
 print r
--- python/darknet.py
+++ python/darknet.py
@@ -119,7 +119,7 @@ if __name__ == "__main__":
     #meta = load_meta("cfg/imagenet1k.data")
     #r = classify(net, meta, im)
     #print r[:10]
-    net = load_net("cfg/tiny-yolo.cfg", "tiny-yolo.backup", 0)
+    net = load_net("cfg/tiny-yolo.cfg", "tiny-yolo.weights", 0)
     meta = load_meta("cfg/coco.data")
     r = detect(net, meta, "data/dog.jpg")
     print r
