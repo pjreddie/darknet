6af2cf692c3a82e4932c16e91280a73f2f3f6a53
AlexeyAB <alexeyab84@gmail.com>
2018-02-15 14:52:51 +0300

    resnet152.cfg

--- build/darknet/x64/classifier_resnet50.cmd
+++ build/darknet/x64/classifier_resnet50.cmd
@@ -1,6 +1,8 @@
 darknet.exe classifier predict cfg/imagenet1k.data cfg/resnet50.cfg resnet50.weights
 
+rem darknet.exe classifier predict cfg/imagenet1k.data cfg/resnet152.cfg resnet152.weights
+
 
 pause
 
-REM Download weights for DenseNet201 and ResNet50 by this link: https://pjreddie.com/darknet/imagenet/
+REM Download weights for DenseNet201, ResNet50 and ResNet152 by this link: https://pjreddie.com/darknet/imagenet/
