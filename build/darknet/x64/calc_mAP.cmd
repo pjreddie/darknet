8474f491f2be64222fb1662b088d87529a22670f
AlexeyAB <alexeyab84@gmail.com>
2018-02-18 19:45:38 +0300

    Minor fix

--- build/darknet/x64/calc_mAP.cmd
+++ build/darknet/x64/calc_mAP.cmd
@@ -1,10 +1,10 @@
 rem # How to calculate mAP (mean average precision)
 
 
-darknet.exe detector map data/voc.data tiny-yolo-voc.cfg tiny-yolo-voc.weights
+rem darknet.exe detector map data/voc.data tiny-yolo-voc.cfg tiny-yolo-voc.weights
 
 
-rem darknet.exe detector map data/voc.data yolo-voc.cfg yolo-voc.weights
+darknet.exe detector map data/voc.data yolo-voc.cfg yolo-voc.weights
 
 
 
