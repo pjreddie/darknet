9f7d403a3cb7dc62c658231f4cf18e33d152806c
AlexeyAB <alexeyab84@gmail.com>
2018-03-28 23:33:03 +0300

    Added Yolo v2 bash files

---
+++ net_cam_v3.sh
@@ -0,0 +1,6 @@
+#rm test_dnn_out.avi
+
+./darknet detector demo ./cfg/coco.data ./cfg/yolov3.cfg ./yolov3.weightss rtsp://admin:admin12345@192.168.0.228:554 -i 0 -thresh 0.25
+
+
+
---
+++ video_v2.sh
@@ -0,0 +1,6 @@
+
+
+./darknet detector demo ./cfg/coco.data ./cfg/yolov2.cfg ./yolov2.weights test50.mp4 -i 0 -thresh 0.25
+
+
+
