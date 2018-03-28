4bd0be410fd2e6126defd867d51c977c274a2900
AlexeyAB <alexeyab84@gmail.com>
2018-03-28 03:27:35 +0300

    One more try

--- src/upsample_layer.h
+++ src/upsample_layer.h
@@ -5,13 +5,13 @@
 #include "network.h"
 
 layer make_upsample_layer(int batch, int w, int h, int c, int stride);
-void forward_upsample_layer(const layer l, network net);
-void backward_upsample_layer(const layer l, network net);
+void forward_upsample_layer(const layer l, network_state state);
+void backward_upsample_layer(const layer l, network_state state);
 void resize_upsample_layer(layer *l, int w, int h);
 
 #ifdef GPU
-void forward_upsample_layer_gpu(const layer l, network net);
-void backward_upsample_layer_gpu(const layer l, network net);
+void forward_upsample_layer_gpu(const layer l, network_state state);
+void backward_upsample_layer_gpu(const layer l, network_state state);
 #endif
 
 #endif
--- src/yolo_layer.h
+++ src/yolo_layer.h
@@ -6,15 +6,15 @@
 #include "network.h"
 
 layer make_yolo_layer(int batch, int w, int h, int n, int total, int *mask, int classes);
-void forward_yolo_layer(const layer l, network net);
-void backward_yolo_layer(const layer l, network net);
+void forward_yolo_layer(const layer l, network_state state);
+void backward_yolo_layer(const layer l, network_state state);
 void resize_yolo_layer(layer *l, int w, int h);
 int yolo_num_detections(layer l, float thresh);
 int get_yolo_detections(layer l, int w, int h, int netw, int neth, float thresh, int *map, int relative, detection *dets, int letter);
 
 #ifdef GPU
-void forward_yolo_layer_gpu(const layer l, network net);
-void backward_yolo_layer_gpu(layer l, network net);
+void forward_yolo_layer_gpu(const layer l, network_state state);
+void backward_yolo_layer_gpu(layer l, network_state state);
 #endif
 
 #endif
