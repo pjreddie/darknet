726cebd3fb67d65ec6d2d49fa6bfba4c053085df
AlexeyAB <alexeyab84@gmail.com>
2018-04-02 15:02:53 +0300

    Fixed detector recall

--- src/detector.c
+++ src/detector.c
@@ -471,7 +471,7 @@ void validate_detector_recall(char *datacfg, char *cfgfile, char *weightfile)
 			++total;
 			box t = { truth[j].x, truth[j].y, truth[j].w, truth[j].h };
 			float best_iou = 0;
-			for (k = 0; k < l.w*l.h*l.n; ++k) {
+			for (k = 0; k < nboxes; ++k) {
 				float iou = box_iou(dets[k].bbox, t);
 				if (dets[k].objectness > thresh && iou > best_iou) {
 					best_iou = iou;
@@ -828,7 +828,7 @@ int anchors_comparator(const void *pa, const void *pb)
 {
 	anchors_t a = *(anchors_t *)pa;
 	anchors_t b = *(anchors_t *)pb;
-	float diff = b.w - a.w;
+	float diff = b.w*b.h - a.w*a.h;
 	if (diff < 0) return 1;
 	else if (diff > 0) return -1;
 	return 0;
