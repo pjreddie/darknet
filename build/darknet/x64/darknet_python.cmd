c9b8bdee1886df5f83973d91c3597c28f99a9e0c
AlexeyAB <alexeyab84@gmail.com>
2018-05-06 21:51:31 +0300

    Minor fix - what pip-packages are required for Python scripts

--- build/darknet/x64/darknet_python.cmd
+++ build/darknet/x64/darknet_python.cmd
@@ -1,6 +1,7 @@
 rem download Python 2.7.14 from: https://www.python.org/downloads/release/python-2714/
 rem C:\Python27\Scripts\pip install numpy
-
+rem C:\Python27\Scripts\pip install scikit-image
+rem C:\Python27\Scripts\pip install scipy
 
 C:\Python27\python.exe darknet.py
 
---
+++ scripts/log_parser/run_log_parser_windows.cmd
@@ -0,0 +1,6 @@
+C:\Python27\Scripts\pip install numpy
+C:\Python27\Scripts\pip install matplotlib
+
+C:\Python27\python.exe log_parser.py --source-dir ./ --save-dir ./ --log-file test.log --show true
+
+pause
