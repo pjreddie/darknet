6c675a09278aeb7f579ff9ee7c20e990119be8ff
Yu Hao <jinyu121@gmail.com>
2018-05-31 14:04:16 +0800

    Fix openCV 3.4.1 highgui

--- src/http_stream.cpp
+++ src/http_stream.cpp
@@ -44,7 +44,7 @@ using std::cerr;
 using std::endl;
 
 #include "opencv2/opencv.hpp"
-#include "opencv2/highgui.hpp"
+#include "opencv2/highgui/highgui.hpp"
 #include "opencv2/highgui/highgui_c.h"
 #include "opencv2/imgproc/imgproc_c.h"
 #ifndef CV_VERSION_EPOCH
