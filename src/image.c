65e1d0ab78764631fdbab2785af9e1a0a4e2fcde
ctmackay <charlestmackay@gmail.com>
2018-07-15 18:40:46 -0700

    write predictions on the detected frames

--- src/image.c
+++ src/image.c
@@ -242,12 +242,16 @@ void draw_detections(image im, detection *dets, int num, float thresh, char **na
 
     for(i = 0; i < num; ++i){
         char labelstr[4096] = {0};
-        int class = -1;
+        char probstr[30];
+        int class_num = -1;
         for(j = 0; j < classes; ++j){
             if (dets[i].prob[j] > thresh){
-                if (class < 0) {
+                if (class_num < 0) {
                     strcat(labelstr, names[j]);
-                    class = j;
+                    snprintf(probstr, sizeof probstr,  " %.2f%%", dets[i].prob[j]*100);
+
+                    strcat(labelstr, probstr);
+                    class_num = j;
                 } else {
                     strcat(labelstr, ", ");
                     strcat(labelstr, names[j]);
@@ -255,7 +259,7 @@ void draw_detections(image im, detection *dets, int num, float thresh, char **na
                 printf("%s: %.0f%%\n", names[j], dets[i].prob[j]*100);
             }
         }
-        if(class >= 0){
+        if(class_num >= 0){
             int width = im.h * .006;
 
             /*
@@ -265,8 +269,8 @@ void draw_detections(image im, detection *dets, int num, float thresh, char **na
                }
              */
 
-            //printf("%d %s: %.0f%%\n", i, names[class], prob*100);
-            int offset = class*123457 % classes;
+            //printf("%d %s: %.0f%%\n", i, names[class_num], prob*100);
+            int offset = class_num*123457 % classes;
             float red = get_color(2,offset,classes);
             float green = get_color(1,offset,classes);
             float blue = get_color(0,offset,classes);
