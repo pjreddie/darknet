e92c635f35619c649d234d4ff59eb5e3d3de1854
AlexeyAB <alexeyab84@gmail.com>
2018-09-27 23:27:14 +0300

    Compile fix

--- src/detector.c
+++ src/detector.c
@@ -25,11 +25,11 @@
 #pragma comment(lib, "opencv_highgui" OPENCV_VERSION ".lib")
 #endif
 
-#include "http_stream.h"
-
 IplImage* draw_train_chart(float max_img_loss, int max_batches, int number_of_lines, int img_size);
 void draw_train_loss(IplImage* img, int img_size, float avg_loss, float max_img_loss, int current_batch, int max_batches);
 #endif    // OPENCV
+
+#include "http_stream.h"
 
 static int coco_ids[] = {1,2,3,4,5,6,7,8,9,10,11,13,14,15,16,17,18,19,20,21,22,23,24,25,27,28,31,32,33,34,35,36,37,38,39,40,41,42,43,44,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,67,70,72,73,74,75,76,77,78,79,80,81,82,84,85,86,87,88,89,90};
 
--- src/http_stream.h
+++ src/http_stream.h
@@ -8,6 +8,7 @@ extern "C" {
 #include "image.h"
 #include <stdint.h>
 
+#ifdef OPENCV
 void send_mjpeg(IplImage* ipl, int port, int timeout, int quality);
 CvCapture* get_capture_webcam(int index);
 CvCapture* get_capture_video_stream(char *path);
@@ -17,6 +18,7 @@ int get_stream_fps_cpp(CvCapture *cap);
 image image_data_augmentation(IplImage* ipl, int w, int h,
     int pleft, int ptop, int swidth, int sheight, int flip,
     float jitter, float dhue, float dsat, float dexp);
+#endif  // OPENCV
 
 double get_time_point();
 void start_timer();
