e0596a10465718d9f39008c8f4b5072314929057
AlexeyAB <alexeyab84@gmail.com>
2019-03-01 16:56:00 +0300

    Added cmd/sh scripts for downloading ILSVRC2012 train & valid for Windows

---
+++ scripts/windows/win_install_cygwin.cmd
@@ -0,0 +1,12 @@
+echo Download file: https://www.cygwin.com/setup-x86_64.exe
+
+
+setup-x86_64.exe -q -P dos2unix,wget,tar,untar,gzip,unzip,qawk,bzip2,git,vim,gcc-g++,make,grep,sed,find
+
+
+# wget rawgit.com/transcode-open/apt-cyg/master/apt-cyg
+# install apt-cyg /bin
+
+echo Finished
+
+pause
---
+++ scripts/windows/windows_imagenet_label.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p labelled
+wd=`pwd`
+
+for f in val/*.xml;
+do
+label=`grep -m1 "<name>" $f | grep -oP '<name>\K[^<]*'`
+im=`echo $f | sed 's/val/imgs/; s/xml/JPEG/'`
+out=`echo $im | sed 's/JPEG/'${label}'.JPEG/; s/imgs/labelled/'`
+mv ${wd}/$im ${wd}/$out
+#ln -s ${wd}/$im ${wd}/$out
+done
+
+#find ${wd}/labelled -name \*.JPEG > inet.val.list
+
