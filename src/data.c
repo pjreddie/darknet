971065d4d6612b0892f2658e6f7e4df419c90791
dongyyyyy <ehddmleheheh@naver.com>
2019-05-13 10:39:47 +0900

    update 5-13

--- src/data.c
+++ src/data.c
@@ -1483,11 +1483,11 @@ void get_next_batch(data d, int n, int offset, float *X, float *y)// get_next_ba
     for(j = 0; j < n; ++j){
         int index = offset + j;
         memcpy(X+j*d.X.cols, d.X.vals[index], d.X.cols*sizeof(float));
-        printf("X+j*d.X.cols = %lf,  d.X.vals[index] = %lf",X+j*d.X.cols, d.X.vals[index]);
-        printf("this size = %d",d.X.cols*sizeof(float));
+        printf("X+j*d.X.cols = %f,  d.X.vals[index] = %f",X+j*d.X.cols, d.X.vals[index]);
+        printf("this size = %ld\n",d.X.cols*sizeof(float));
         if(y) memcpy(y+j*d.y.cols, d.y.vals[index], d.y.cols*sizeof(float));
-        printf("y+j*d.y.cols = %lf,  d.y.vals[index] = %lf",X+j*d.y.cols, d.y.vals[index]);
-        printf("this size = %d",d.y.cols*sizeof(float));
+        printf("y+j*d.y.cols = %f,  d.y.vals[index] = %f",X+j*d.y.cols, d.y.vals[index]);
+        printf("this size = %ld\n",d.y.cols*sizeof(float));
     }
 }
 
