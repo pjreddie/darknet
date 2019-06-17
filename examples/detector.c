fdaadfe2da28addd4fa92bdffe6ebb627204deab
dongyyyyy <ehddmleheheh@naver.com>
2019-06-17 18:53:48 +0900

    update

--- examples/detector.c
+++ examples/detector.c
@@ -1,5 +1,5 @@
 #include "darknet.h"
-#include <windows.h>
+#include <unistd.h>
 static int coco_ids[] = {1,2,3,4,5,6,7,8,9,10,11,13,14,15,16,17,18,19,20,21,22,23,24,25,27,28,31,32,33,34,35,36,37,38,39,40,41,42,43,44,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,67,70,72,73,74,75,76,77,78,79,80,81,82,84,85,86,87,88,89,90};
 int cando = 0;
 
@@ -815,6 +815,7 @@ void network_detect(network *net, image im, float thresh, float hier_thresh, flo
 }
 */
 
+//추가 함수 ( 사진을 모델을 읽은 후 사진 읽기를 대기하는 함수 )
 void detector_run(char *datacfg, char *cfgfile, char *weightfile, char *filename, float thresh, float hier_thresh, char *outfile, int fullscreen)
 {
     list *options = read_data_cfg(datacfg);
