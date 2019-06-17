67e70cff713f1f43c89c11e1a3e5140043c199c9
dongyyyyy <ehddmleheheh@naver.com>
2019-06-17 18:56:55 +0900

    update

--- examples/detector.c
+++ examples/detector.c
@@ -844,10 +844,10 @@ void detector_run(char *datacfg, char *cfgfile, char *weightfile, char *filename
             strtok(input, "\n");
         }
         */
-        for(j = 1 ; j <= 10 ; j++){
-        sprintf(input,"/home/kdy/information/TestImage/Test_%d.jpg",j);
         image im = load_image_color(input,0,0);
         image sized = letterbox_image(im, net->w, net->h);
+        for(j = 1 ; j <= 10 ; j++){
+        sprintf(input,"/home/kdy/information/TestImage/Test_%d.jpg",j);
         //image sized = resize_image(im, net->w, net->h);
         //image sized2 = resize_max(im, net->w);
         //image sized = crop_image(sized2, -((net->w - sized2.w)/2), -((net->h - sized2.h)/2), net->w, net->h);
