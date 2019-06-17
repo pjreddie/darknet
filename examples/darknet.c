7ef76449afe673f98170edb1a6b7397136b93c14
dongyyyyy <ehddmleheheh@naver.com>
2019-06-17 17:38:54 +0900

    update

--- examples/darknet.c
+++ examples/darknet.c
@@ -6,7 +6,7 @@
 
 extern void predict_classifier(char *datacfg, char *cfgfile, char *weightfile, char *filename, int top);
 extern void test_detector(char *datacfg, char *cfgfile, char *weightfile, char *filename, float thresh, float hier_thresh, char *outfile, int fullscreen);
-extern void void detector_run(char *datacfg, char *cfgfile, char *weightfile, char *filename, float thresh, float hier_thresh, char *outfile, int fullscreen);
+extern void detector_run(char *datacfg, char *cfgfile, char *weightfile, char *filename, float thresh, float hier_thresh, char *outfile, int fullscreen);
 extern void run_yolo(int argc, char **argv);
 extern void run_detector(int argc, char **argv); // darknet.h ?ôï?ù∏
 extern void run_coco(int argc, char **argv);
