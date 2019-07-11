4b0e2b8f0e7d85496cb10db79c6dda58dc7403bb
dongyyyyy <dongyoung0218@gmail.com>
2019-07-11 16:35:48 +0900

    update

--- src/draw_opencv.cpp
+++ src/draw_opencv.cpp
@@ -1,8 +1,8 @@
 #ifdef OPENCV
 #include "darknet.h"
-#include "opencv2/opencv.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/core.hpp"
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/core.hpp>
 #include <iostream>
 #include <cstdio>
 #include <time.h>
