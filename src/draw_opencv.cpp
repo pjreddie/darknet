ab40c22789eb36d0def096dae685b1d5e0b9a9ef
dongyyyyy <dongyoung0218@gmail.com>
2019-07-11 17:18:48 +0900

    update

--- src/draw_opencv.cpp
+++ src/draw_opencv.cpp
@@ -48,7 +48,7 @@ void load_mat_image_point(char *input)
 	{
 		return;
 	}
-    destroyAllWiondow("Original");
+    destroyAllWiondows();
 }
 void onMouseCheck(int event, int x, int y, int flags, void* param)
 {
