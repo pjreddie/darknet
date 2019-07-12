ede33fc8979bf0e06b9e12a5108518acf80ea00b
dongyyyyy <dongyoung0218@gmail.com>
2019-07-12 14:11:55 +0900

    update

--- src/draw_opencv.cpp
+++ src/draw_opencv.cpp
@@ -299,7 +299,7 @@ int check_person_point(int px,int py,Points *ary)
 		j = (i + 1) % ary->size;
 		if ((ary->y[i] > py) != (ary->y[j] > py)) // 두 좌표(연결점)의 y좌표가 점의 좌표와 교차할 경우만 확인
 		{
-			double atX = (ary->.x[j] - ary->x[i]) * (py - ary->y[i]) / (ary->y[j] - ary->.y[i]) + ary->x[i];
+			double atX = (ary->x[j] - ary->x[i]) * (py - ary->y[i]) / (ary->y[j] - ary->y[i]) + ary->x[i];
 			if (px <= atX)
 				crosses++;
 		}
