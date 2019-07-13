57acdde277c3780546d339a999537f0508b9a26f
dongyyyyy <dongyoung0218@gmail.com>
2019-07-13 15:39:59 +0900

    update

--- src/image_opencv.cpp
+++ src/image_opencv.cpp
@@ -207,7 +207,7 @@ image draw_polygonlines(image im,Points *ary)
     }
     if(size >=3 )
     {
-        polylines(image, &points, &size, 1, true, Scalar(255, 0, 0)); 
+        polylines(&image, &points, &size, 1, true, Scalar(255, 0, 0)); 
     }
     im = mat_to_image(image);
     return im;
