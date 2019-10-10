e9e896e7e531083bed2e2b73bc29530424a3c579
dongyyyyy <dongyoung0218@gmail.com>
2019-10-10 12:34:04 +0900

    update image.c

--- src/image.c
+++ src/image.c
@@ -1,10 +1,10 @@
-#include "darknet.h"
 #include "image.h"
 #include "utils.h"
 #include "blas.h"
 #include "cuda.h"
 #include <stdio.h>
 #include <math.h>
+#include "darknet.h"
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
