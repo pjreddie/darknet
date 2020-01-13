f83bed9173a377a4ba9605ab86190d7a1443a596
TaoSun <taosun@mit.edu>
2020-01-13 00:08:27 -0500

    makefile and opencv file update
    Change the makefile and update image_opencv.cpp

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 GPU=0
 CUDNN=0
-OPENCV=0
+OPENCV=1
 OPENMP=0
 DEBUG=0
 
@@ -20,48 +20,49 @@ EXEC=darknet
 OBJDIR=./obj/
 
 CC=gcc
-CPP=g++
-NVCC=nvcc 
+CPP=g++ -std=c++11
+NVCC=nvcc
 AR=ar
 ARFLAGS=rcs
 OPTS=-Ofast
-LDFLAGS= -lm -pthread 
+LDFLAGS= -lm -pthread
 COMMON= -Iinclude/ -Isrc/
-CFLAGS=-Wall -Wno-unused-result -Wno-unknown-pragmas -Wfatal-errors -fPIC
+CFLAGS=-Wall -Wno-unused-result -Wno-unknown-pragmas -Wfatal-errors -fPIC -ljson-c
 
-ifeq ($(OPENMP), 1) 
+ifeq ($(OPENMP), 1)
 CFLAGS+= -fopenmp
 endif
 
-ifeq ($(DEBUG), 1) 
+ifeq ($(DEBUG), 1)
 OPTS=-O0 -g
 endif
 
 CFLAGS+=$(OPTS)
 
-ifeq ($(OPENCV), 1) 
+ifeq ($(OPENCV), 1)
 COMMON+= -DOPENCV
 CFLAGS+= -DOPENCV
-LDFLAGS+= `pkg-config --libs opencv` -lstdc++
-COMMON+= `pkg-config --cflags opencv` 
+LDFLAGS+= `pkg-config --libs opencv4` -lstdc++
+COMMON+= `pkg-config --cflags opencv4`
 endif
 
-ifeq ($(GPU), 1) 
+
+ifeq ($(GPU), 1)
 COMMON+= -DGPU -I/usr/local/cuda/include/
 CFLAGS+= -DGPU
 LDFLAGS+= -L/usr/local/cuda/lib64 -lcuda -lcudart -lcublas -lcurand
 endif
 
-ifeq ($(CUDNN), 1) 
-COMMON+= -DCUDNN 
+ifeq ($(CUDNN), 1)
+COMMON+= -DCUDNN
 CFLAGS+= -DCUDNN
 LDFLAGS+= -lcudnn
 endif
 
 OBJ=gemm.o utils.o cuda.o deconvolutional_layer.o convolutional_layer.o list.o image.o activations.o im2col.o col2im.o blas.o crop_layer.o dropout_layer.o maxpool_layer.o softmax_layer.o data.o matrix.o network.o connected_layer.o cost_layer.o parser.o option_list.o detection_layer.o route_layer.o upsample_layer.o box.o normalization_layer.o avgpool_layer.o layer.o local_layer.o shortcut_layer.o logistic_layer.o activation_layer.o rnn_layer.o gru_layer.o crnn_layer.o demo.o batchnorm_layer.o region_layer.o reorg_layer.o tree.o  lstm_layer.o l2norm_layer.o yolo_layer.o iseg_layer.o image_opencv.o
 EXECOBJA=captcha.o lsd.o super.o art.o tag.o cifar.o go.o rnn.o segmenter.o regressor.o classifier.o coco.o yolo.o detector.o nightmare.o instance-segmenter.o darknet.o
-ifeq ($(GPU), 1) 
-LDFLAGS+= -lstdc++ 
+ifeq ($(GPU), 1)
+LDFLAGS+= -lstdc++
 OBJ+=convolutional_kernels.o deconvolutional_kernels.o activation_kernels.o im2col_kernels.o col2im_kernels.o blas_kernels.o crop_layer_kernels.o dropout_layer_kernels.o maxpool_layer_kernels.o avgpool_layer_kernels.o
 endif
 
@@ -102,4 +103,3 @@ results:
 
 clean:
 	rm -rf $(OBJS) $(SLIB) $(ALIB) $(EXEC) $(EXECOBJ) $(OBJDIR)/*
-
--- src/image_opencv.cpp
+++ src/image_opencv.cpp
@@ -1,15 +1,15 @@
 #ifdef OPENCV
-
+ 
 #include "stdio.h"
 #include "stdlib.h"
 #include "opencv2/opencv.hpp"
 #include "image.h"
-
+ 
 using namespace cv;
-
+ 
 extern "C" {
-
-IplImage *image_to_ipl(image im)
+ 
+/*IplImage *image_to_ipl(image im)
 {
     int x,y,c;
     IplImage *disp = cvCreateImage(cvSize(im.w,im.h), IPL_DEPTH_8U, im.c);
@@ -24,7 +24,6 @@ IplImage *image_to_ipl(image im)
     }
     return disp;
 }
-
 image ipl_to_image(IplImage* src)
 {
     int h = src->height;
@@ -34,7 +33,6 @@ image ipl_to_image(IplImage* src)
     unsigned char *data = (unsigned char *)src->imageData;
     int step = src->widthStep;
     int i, j, k;
-
     for(i = 0; i < h; ++i){
         for(k= 0; k < c; ++k){
             for(j = 0; j < w; ++j){
@@ -43,41 +41,71 @@ image ipl_to_image(IplImage* src)
         }
     }
     return im;
-}
-
+}*/
+ 
 Mat image_to_mat(image im)
 {
     image copy = copy_image(im);
     constrain_image(copy);
     if(im.c == 3) rgbgr_image(copy);
-
-    IplImage *ipl = image_to_ipl(copy);
+ 
+	int x, y, c;
+	Mat m = Mat(Size(im.w, im.h), CV_8UC(im.c));
+	int step = m.step;
+ 
+	for (y = 0; y < im.h; ++y) {
+			for (x = 0; x < im.w; ++x) {
+					for (c = 0; c < im.c; ++c) {
+							float val = copy.data[c*im.h*im.w + y * im.w + x];
+							m.data[y*step + x * im.c + c] = (unsigned char)(val * 255);
+					}
+			}
+	}
+ 
+/*    IplImage *ipl = image_to_ipl(copy);
     Mat m = cvarrToMat(ipl, true);
-    cvReleaseImage(&ipl);
+    cvReleaseImage(&ipl);*/
     free_image(copy);
     return m;
 }
-
+ 
 image mat_to_image(Mat m)
 {
-    IplImage ipl = m;
+/*    IplImage ipl = m;
     image im = ipl_to_image(&ipl);
-    rgbgr_image(im);
+    rgbgr_image(im);*/
+	int h = m.rows;
+	int w = m.cols;
+	int c = m.channels();
+	image im = make_image(w, h, c);
+	unsigned char *data = (unsigned char *)m.data;
+	int step = m.step;
+	int i, j, k;
+ 
+	for (i = 0; i < h; ++i) {
+			for (k = 0; k < c; ++k) {
+					for (j = 0; j < w; ++j) {
+							im.data[k*w*h + i * w + j] = data[i*step + j * c + k] / 255.;
+					}
+			}
+	}
+    if (im.c == 3) rgbgr_image(im);
+	
     return im;
 }
-
+ 
 void *open_video_stream(const char *f, int c, int w, int h, int fps)
 {
     VideoCapture *cap;
     if(f) cap = new VideoCapture(f);
     else cap = new VideoCapture(c);
     if(!cap->isOpened()) return 0;
-    if(w) cap->set(CV_CAP_PROP_FRAME_WIDTH, w);
-    if(h) cap->set(CV_CAP_PROP_FRAME_HEIGHT, w);
-    if(fps) cap->set(CV_CAP_PROP_FPS, w);
+    if(w) cap->set(CAP_PROP_FRAME_WIDTH, w);
+    if(h) cap->set(CAP_PROP_FRAME_HEIGHT, w);
+    if(fps) cap->set(CAP_PROP_FPS, w);
     return (void *) cap;
 }
-
+ 
 image get_image_from_stream(void *p)
 {
     VideoCapture *cap = (VideoCapture *)p;
@@ -86,7 +114,7 @@ image get_image_from_stream(void *p)
     if(m.empty()) return make_empty_image(0,0,0);
     return mat_to_image(m);
 }
-
+ 
 image load_image_cv(char *filename, int channels)
 {
     int flag = -1;
@@ -109,7 +137,7 @@ image load_image_cv(char *filename, int channels)
     image im = mat_to_image(m);
     return im;
 }
-
+ 
 int show_image_cv(image im, const char* name, int ms)
 {
     Mat m = image_to_mat(im);
@@ -118,18 +146,18 @@ int show_image_cv(image im, const char* name, int ms)
     if (c != -1) c = c%256;
     return c;
 }
-
+ 
 void make_window(char *name, int w, int h, int fullscreen)
 {
     namedWindow(name, WINDOW_NORMAL); 
     if (fullscreen) {
-        setWindowProperty(name, CV_WND_PROP_FULLSCREEN, CV_WINDOW_FULLSCREEN);
+        setWindowProperty(name, WND_PROP_FULLSCREEN, WINDOW_FULLSCREEN);
     } else {
         resizeWindow(name, w, h);
         if(strcmp(name, "Demo") == 0) moveWindow(name, 0, 0);
     }
 }
-
+ 
 }
-
+ 
 #endif
