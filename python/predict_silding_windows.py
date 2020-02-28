a1f0b3d555d82db7ede3b7a61b655e843c6b4049
Zhenning Yang <47701932+zyang37@users.noreply.github.com>
2020-02-28 16:41:51 -0500

    update predict_window

--- python/predict_silding_windows.py
+++ python/predict_silding_windows.py
@@ -17,13 +17,32 @@ import image_slicer
 from darknet import *
 from image_processing import *
 
-#if '/Users/zhenningyang/Documents/tiler' not in sys.path:
-#    sys.path.insert(0, '/Users/zhenningyang/Documents/tiler')
+#if '/home/vj/Documents/tiler' not in sys.path:
+#    sys.path.insert(0, '/home/vj/Documents/tiler')
 if '{}/../tiler/'.format(os.getcwd()) not in sys.path:
     sys.path.insert(0, '{}/../tiler/'.format(os.getcwd()))
-
 from tiler import *
 from matplotlib import pyplot as plt
+
+def distance(box1, box2):
+    x = (box1[0] - box2[0])**2
+    y = (box1[1] - box2[1])**2
+    return (x+y)**0.5
+
+def draw_cor(path, boxes):
+    img = cv2.imread('{}'.format(path))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    fontScale = 0.5
+    color = (255, 20, 147)
+    thickness = 2
+    for box in boxes:
+        print("box",box, type(box))
+        org = (int(box[2]), int(box[3]*0.98))
+        cv2.rectangle(img, (box[0], box[1]), (box[2], box[3]), color, 2)
+        img = cv2.putText(img, 'car_plate', org, font, fontScale, color, thickness, cv2.LINE_AA)
+
+    return img
 
 def from_yolo_to_cor(img, box):
     img_h, img_w, _ = img.shape
@@ -40,7 +59,7 @@ def draw(path, r):
     font = cv2.FONT_HERSHEY_SIMPLEX
     fontScale = 0.5
     color = (255, 20, 147)
-    thickness = 1
+    thickness = 2
 
     for i in range(len(r)):
         #print(r[i][2])
@@ -88,11 +107,12 @@ if __name__=="__main__":
     net = load_net(b"../cfg/yolov3_ssig.cfg", b"../../../Documents/darknet/backup/yolov3_ssig_final.weights", 0)
     meta = load_meta(b"../cfg/ssig.data")
 
-    temp = 'temp.jpg'
+    temp = 'temp/temp.jpg'
     pred_temp = 'pred.jpg'
 
     img = read_image(sys.argv[1])
     boxes = image_boxes(img, [416,416])
+    org_boxes = []
 
     for i in range(len(boxes)):
         im = cv2.cvtColor(boxes[i]['img'], cv2.COLOR_BGR2RGB)
@@ -104,22 +124,44 @@ if __name__=="__main__":
 
             # predict
             r = detect(net, meta, bytes(temp, 'utf-8'))
+            #print(r)
             img = draw("{}".format(temp), r)
             img = Image.fromarray(img)
             img.save(pred_temp)
 
             # plt read pred.jpg
             im = cv2.imread(pred_temp)
-            cv2.imshow('res: '+str(im.shape)+' _ '+'pos: '+str(boxes[i]['pos']), im)
-            key = cv2.waitKey(1)
+            imb = cv2.resize(im, (600,600))
+            cv2.imshow('res: '+str(im.shape)+' _ '+'pos: '+str(boxes[i]['pos']), imb)
+            key = cv2.waitKey(300)
             cv2.destroyAllWindows()
-            #im = plt.imread(pred_temp)
-            #plt.figure(figsize=(5,5))
-            #plt.imshow(im)
-            #plt.xticks([])
-            #plt.yticks([])
-            #plt.savefig('temp.png')
-            #plt.title('res: '+str(im.shape)+'\n'+'pos: '+str(boxes[i]['pos']))
-            #plt.show()
+
+            # save boxes
+            for j in range(len(r)):
+                box = []
+                box_temp = from_yolo_to_cor(im, r[j][2])
+                box.append(box_temp[0] + boxes[i]['pos'][0])
+                box.append(box_temp[1] + boxes[i]['pos'][1])
+                box.append(box_temp[2] + boxes[i]['pos'][0])
+                box.append(box_temp[3] + boxes[i]['pos'][1])
+
+                org_boxes.append(box)
+                for b in org_boxes:
+                    d = distance(b, box)
+                    print(d)
+                    if d<20. and d!=0.0:
+                        org_boxes.pop()
+
             if key == ord('q'):
                 break
+
+    print(org_boxes)
+    print("img", img)
+    img = draw_cor(sys.argv[1], org_boxes)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    cv2.imshow('ouput', img)
+    key = cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = Image.fromarray(img)
+    img.save('output/output_tiling.jpg')
