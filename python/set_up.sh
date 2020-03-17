3f04a578de2f2d070c8debde916e9073b982cff2
Zhenning Yang <zhenningyang@pop-os.localdomain>
2020-03-16 21:14:42 -0400

    add sh

---
+++ python/set_up.sh
@@ -0,0 +1,10 @@
+apt-get update
+# apt install git-all
+apt-get install python3
+apt-get install python
+apt install curl
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
+apt install python3-pip
+apt-get install -y libsm6 libxext6 libxrender-dev
+apt-get update
