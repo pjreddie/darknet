c36052bedb52e8b206d942e2fadf1deeb70e3175
Iordanis Kostelidis <kostelidis@ieee.org>
2020-06-17 22:34:32 +0300

    adds docker support for cpu, opencv, openmp and cuda

---
+++ docker/access-gpu-container.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cur_dir=$(pwd | rev | cut -d"/" -f1 | rev)
+
+if [[ "$cur_dir" == "docker" ]]; then
+  cd ..
+fi
+
+if ! [[ -f "./docker/.env" ]]; then
+    cp ./docker/.env.example ./docker/.env
+fi
+
+source ./docker/.env
+
+docker run \
+--gpus all \
+--rm -it \
+--privileged \
+--net=host \
+--ipc=host \
+-e DISPLAY=$DISPLAY \
+-v /tmp/.X11-unix:/tmp/.X11-unix \
+-v /dev/"${DOCKER_CAMERA_DEVICE}":/dev/video0 \
+-v $(pwd):/home/darknet/app \
+darknet:gpu-latest \
+bash
