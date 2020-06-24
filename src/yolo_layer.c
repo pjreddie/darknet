5963c22adf5cc892929280fdc7f309c999e87f78
Juuso Alasuutari <juuso.alasuutari@gmail.com>
2020-06-24 16:20:20 +0300

    Fix '"/*" within comment' warnings; remove instances of '//*' so as to not confuse some syntax highlighters
    Signed-off-by: Juuso Alasuutari <juuso.alasuutari@gmail.com>

--- src/yolo_layer.c
+++ src/yolo_layer.c
@@ -597,7 +597,7 @@ void forward_yolo_layer(const layer l, network_state state)
     if (count == 0) count = 1;
     if (class_count == 0) class_count = 1;
 
-    //*(l.cost) = pow(mag_array(l.delta, l.outputs * l.batch), 2);
+    //(*l.cost) = pow(mag_array(l.delta, l.outputs * l.batch), 2);
     //printf("Region %d Avg IOU: %f, Class: %f, Obj: %f, No Obj: %f, .5R: %f, .75R: %f,  count: %d\n", state.index, avg_iou / count, avg_cat / class_count, avg_obj / count, avg_anyobj / (l.w*l.h*l.n*l.batch), recall / count, recall75 / count, count);
 
     int stride = l.w*l.h;
