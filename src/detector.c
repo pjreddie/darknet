eaee5060d55377f566310b9dc84e5605e1cccccf
Akash Patel <17132214+acxz@users.noreply.github.com>
2022-06-08 09:38:53 -0400

    - Fix warning -Werror=format-security (#8525)
    Co-authored-by: Jeremy Prater <silverevo03@gmail.com>

--- src/detector.c
+++ src/detector.c
@@ -492,7 +492,7 @@ static void print_cocos(FILE *fp, char *image_path, detection *dets, int num_box
             if (dets[i].prob[j] > 0) {
                 char buff[1024];
                 sprintf(buff, "{\"image_id\":%d, \"category_id\":%d, \"bbox\":[%f, %f, %f, %f], \"score\":%f},\n", image_id, coco_ids[j], bx, by, bw, bh, dets[i].prob[j]);
-                fprintf(fp, buff);
+                fprintf(fp, "%s", buff);
                 //printf("%s", buff);
             }
         }
